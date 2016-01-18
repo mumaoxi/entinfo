6c58d2aa1aad5e6066796700b689b38257aa8483
liuxi <xi.liu@abcomb.com>
2016-01-18 09:57:35 +0800

    no message

--- spec/entinfo_spec.rb
+++ spec/entinfo_spec.rb
@@ -6,6 +6,12 @@ describe Entinfo do
   end
 
   it 'does something useful' do
-    expect(false).to eq(true)
+    Entinfo.configure do |config|
+        # 运营商分配给您的SN号(帐号)
+        config.sn = 'SDK-xxx-00-00000'
+        # 运营商分配给您的密码
+        config.pwd = '0-0_0-0'
+    end
+    expect(Entinfo.send_sms('15201280641','早上好！【首趣商城】').has_key?(:success)).to eq(true)
   end
 end
